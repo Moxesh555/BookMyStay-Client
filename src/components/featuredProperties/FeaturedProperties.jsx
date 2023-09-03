4198742598b897af384f0f93677108b1501c84ce
Moxesh555 <2020uec1080@mnit.ac.in>
2023-09-03 15:05:40 +0530

    comm

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -10,25 +10,6 @@ const FeaturedProperties = () => {
         "Loading please wait..."
       ) : (
         <>
-          {/* {data ? (data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
-            </div>
-          ))) :
-            <>
-            </>
-          } */}
         </>
       )}
     </div>
